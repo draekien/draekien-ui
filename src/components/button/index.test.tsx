6c5c117ea322a0ea4ab86700581dd50b47d6ac7b
William Pei <william.h.pei@gmail.com>
2020-11-08 12:17:11 +1100

    Update button component

--- src/components/button/index.test.tsx
+++ src/components/button/index.test.tsx
@@ -1,5 +1,7 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
 import { render } from '@testing-library/react';
-import React from 'react';
+import { jsx } from 'theme-ui';
 import Button from '.';
 
 describe('Button component', () => {
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,25 +1,39 @@
+/** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, Button as ThemeUiButton } from 'theme-ui';
 import * as React from 'react';
 import * as styles from './index.styles';
 
-export type ButtonProps = {
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: styles.ButtonVariant;
   size?: styles.ButtonSize;
+  disabled?: boolean;
+  fullWidth?: boolean;
+  isActive?: boolean;
   onClick?: () => void;
   children: React.ReactNode;
-};
+}
 
 export const Button: React.FC<ButtonProps> = ({
   variant = 'primary',
   size = 'md',
+  disabled = false,
+  fullWidth = false,
+  isActive = false,
   onClick = () => {},
   children,
   ...props
 }) => {
   return (
     <ThemeUiButton
-      sx={styles.buttonCss({ variant, size })}
+      sx={styles.buttonCss({
+        variant,
+        size,
+        disabled,
+        fullWidth,
+        active: isActive,
+      })}
       onClick={onClick}
       {...props}
     >
