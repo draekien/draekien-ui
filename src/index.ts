2b27d02ee54232349d1184f7bb688e0c229e8d23
William Pei <draekien@users.noreply.github.com>
2020-11-08 19:44:35 +1100

    Feature/update scripts (#6)
    * Move storybook static folder to trash
    
    * Update gitignore
    
    * Update lint staged

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { ThemeProvider, ThemeProviderProps } from './components/theme';
+
 export * from './components/button';
+export * from './components/icon';
 
 export { ThemeProvider };
 export type { ThemeProviderProps };
