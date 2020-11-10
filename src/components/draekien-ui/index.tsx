3955bcf294162cf7663b721d0ffe771d057f581c
William Pei <william.h.pei@gmail.com>
2020-11-10 16:14:53 +1100

    Add toast context to ui component

--- src/components/draekien-ui/index.tsx
+++ src/components/draekien-ui/index.tsx
@@ -1,16 +1,49 @@
 import * as React from 'react';
 import { ThemeProvider } from '../theme';
 import { ThemeType } from '../theme/theme';
+import { ToastContainer } from '../toast/container';
+import { ToastProvider } from '../toast/provider';
 
 export type DraekienUiProps = {
   /** any theme overrides */
   theme?: Partial<ThemeType>;
+  /** should the toast context be used
+   * @default true
+   */
+  useToastContext?: boolean;
+  /** offset top of toast container. Required if using toast context
+   * @default '0'
+   */
+  toastContainerOffsetTop?: string | string[];
   /** the main application */
   children: React.ReactNode;
 };
 
-export const DraekienUi: React.FC<DraekienUiProps> = ({ theme, children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+export const DraekienUi: React.FC<DraekienUiProps> = (props) => {
+  const {
+    theme,
+    children,
+    useToastContext = true,
+    toastContainerOffsetTop = '0',
+  } = props;
+  const ConditionalToastContext = ({ children }: any) => {
+    if (useToastContext && toastContainerOffsetTop) {
+      return (
+        <ToastProvider>
+          <ToastContainer offsetTop={toastContainerOffsetTop} />
+          {children}
+        </ToastProvider>
+      );
+    }
+
+    return children;
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ConditionalToastContext>{children}</ConditionalToastContext>
+    </ThemeProvider>
+  );
 };
 
 export default DraekienUi;
