ed1a7d230773a1ef3d0c9f14c7abc577997c4edb
William Pei <william.h.pei@gmail.com>
2020-11-10 18:22:38 +1100

    Add toast tests

---
+++ src/components/toast/index.test.tsx
@@ -0,0 +1,41 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { fireEvent, getByText, render } from '@testing-library/react';
+import { jsx } from 'theme-ui';
+import Toast from '.';
+
+describe('Toast component', () => {
+  test('should match snapshot', () => {
+    const { container } = render(<Toast title="test" message="test" />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should match snapshot when information', () => {
+    const { container } = render(
+      <Toast color="information" title="test" message="test" openFrom="left" />
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('onClick successfully calls callback function', (done) => {
+    const fn = jest.fn(() => done());
+    const { container } = render(<Toast onClick={fn} title="test" />);
+
+    fireEvent(
+      getByText(container, 'test'),
+      new MouseEvent('click', { bubbles: true, cancelable: true })
+    );
+  });
+
+  test('onClose successfully calls callback function', (done) => {
+    const fn = jest.fn(() => done());
+    const { container } = render(<Toast onClose={fn} title="test" />);
+
+    fireEvent(
+      getByText(container, 'close'),
+      new MouseEvent('click', { bubbles: true, cancelable: true })
+    );
+  });
+});
